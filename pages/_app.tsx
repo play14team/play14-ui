eccc72c797ad371f364fcc46d4a4ca7434544183
Cédric Pontet <cedric.pontet@gmail.com>
2022-03-12 17:59:21 +0100

    Changed default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "../styles/styles.scss";
 import "bootstrap/dist/css/bootstrap.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
