64d5ebc1d33f49f2f489082f1ee549dfad6f7459
Cédric Pontet <cedric.pontet@gmail.com>
2022-03-12 19:59:27 +0100

    Moved logos

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,7 @@
 import Container from "./container";
 import styles from "../../styles/Home.module.css";
 import Image from "next/image";
+import logo from "../../public/logo/play14_white_bg_transparent.svg";
 
 const Footer = () => {
   return (
@@ -9,12 +10,7 @@ const Footer = () => {
         <div>
           <h3>We believe in playfulness</h3>
           <span className={styles.logo}>
-            <Image
-              src="/play14_white_bg_transparent.svg"
-              alt="#play14"
-              width={120}
-              height={50}
-            />
+            <Image src={logo} alt="#play14" width={120} height={50} />
           </span>
         </div>
       </Container>
