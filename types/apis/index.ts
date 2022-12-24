1337b55d18cc036921e76145592036965d69d3c2
Cédric PONTET <cpontet@agilepartner.net>
2022-12-24 15:50:07 +0100

    generate types from open api file

---
+++ types/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from "./EventApi";
+export * from "./EventLocationApi";
+export * from "./PlayerApi";
+export * from "./SponsorApi";
+export * from "./UploadFileApi";
+export * from "./UsersPermissionsAuthApi";
+export * from "./UsersPermissionsUsersRolesApi";
+export * from "./VenueApi";
