6250595e44ffd4fb7eedb92f21f743202c6b8636
Cédric PONTET <cpontet@agilepartner.net>
2023-01-12 22:25:09 +0100

    parameterized strapi url ith env var

--- codegen.ts
+++ codegen.ts
@@ -1,14 +1,25 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 
+const STRAPI_SERVER =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
+
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:1337/graphql",
+  schema: STRAPI_SERVER + "/graphql",
   documents: ["pages/**/*.tsx", "components/**/*.tsx"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     "./models/": {
       preset: "client",
-      plugins: [],
+      plugins: [
+        {
+          add: {
+            content: "/* eslint-disable */",
+          },
+        },
+        "typescript",
+        // "typescript-resolvers"
+      ],
     },
   },
 };
