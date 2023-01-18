ee479e3da9966f6d62bb972798267381c3b234e7
Cédric PONTET <cpontet@agilepartner.net>
2023-01-18 08:31:06 +0100

    fixed build issue

--- components/events/schedule.tsx
+++ components/events/schedule.tsx
@@ -15,31 +15,32 @@ const EventSchedule = (props: {
               <h3>{item.day}</h3>
               {item.description}
               <ul>
-                {item.timeslots.map((slot) => {
-                  return (
-                    slot && (
-                      <li>
-                        <Link
-                          href="#"
-                          className="d-flex justify-content-between align-items-center"
-                          onClick={(e) => e.preventDefault()}
-                        >
-                          <span className="courses-name">
-                            {slot.description}
-                          </span>
-                          <div className="courses-meta">
-                            <span className="status">
-                              <i className="bx bx-alarm"></i>
+                {item.timeslots &&
+                  item.timeslots.map((slot) => {
+                    return (
+                      slot && (
+                        <li>
+                          <Link
+                            href="#"
+                            className="d-flex justify-content-between align-items-center"
+                            onClick={(e) => e.preventDefault()}
+                          >
+                            <span className="courses-name">
+                              {slot.description}
                             </span>
-                            <span className="duration">
-                              {slot.time.substring(0, 5)}
-                            </span>
-                          </div>
-                        </Link>
-                      </li>
-                    )
-                  );
-                })}
+                            <div className="courses-meta">
+                              <span className="status">
+                                <i className="bx bx-alarm"></i>
+                              </span>
+                              <span className="duration">
+                                {slot.time.substring(0, 5)}
+                              </span>
+                            </div>
+                          </Link>
+                        </li>
+                      )
+                    );
+                  })}
               </ul>
               <br />
             </div>
