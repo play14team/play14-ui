945d2655a99e9b22ede7e1efd4dc7bbf71a89954
Cédric PONTET <cpontet@agilepartner.net>
2023-02-20 00:29:31 +0100

    dynamic story page

--- graphql/apollo.tsx
+++ graphql/apollo.tsx
@@ -4,16 +4,16 @@ import { PropsWithChildren } from "react";
 const STRAPI_SERVER =
   process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
 const GRAPHQL_ENDPOINT = STRAPI_SERVER + "/graphql";
+const client = new ApolloClient({
+  uri: GRAPHQL_ENDPOINT,
+  cache: new InMemoryCache(),
+});
 
 const StrapiApolloProvider: React.FC<PropsWithChildren<{}>> = ({
   children,
 }) => {
-  const client = new ApolloClient({
-    uri: GRAPHQL_ENDPOINT,
-    cache: new InMemoryCache(),
-  });
-
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
+export { client };
 export default StrapiApolloProvider;
--- libs/apollo-client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_STRAPI_API_URL + "/graphql",
-  cache: new InMemoryCache(),
-});
-
-export default client;
