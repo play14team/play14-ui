0430aecf184e89f933acb7684630577e02e6cc81
Cédric PONTET <cpontet@agilepartner.net>
2023-03-02 20:53:26 +0100

    player details static gen

--- pages/players/[slug].tsx
+++ pages/players/[slug].tsx
@@ -1,25 +1,53 @@
 import { useQuery } from "@apollo/client";
-import type { NextPage } from "next";
+import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import Page from "../../components/layout/page";
 import PlayerDetails from "../../components/players/details";
-import { Player, PlayerDocument } from "../../models/graphql";
+import { client } from "../../graphql/apollo";
+import {
+  Player,
+  PlayerDocument,
+  PlayerSlugsDocument,
+} from "../../models/graphql";
 
-const PlayerDetailsPage: NextPage = () => {
-  const router = useRouter();
-  const slug = router.query.slug as string;
-  const { data, loading, error } = useQuery(PlayerDocument, {
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data } = await client.query({ query: PlayerSlugsDocument });
+
+  const paths = data.players?.data?.map((s) => {
+    const { slug } = s.attributes;
+    return {
+      params: { slug },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<{ player: Player }> = async (
+  context
+) => {
+  const { slug } = context.params;
+  const { data } = await client.query({
+    query: PlayerDocument,
     variables: { slug: slug },
   });
 
   const player = data?.players?.data[0].attributes as Player;
 
+  return {
+    props: { player: player },
+  };
+};
+
+const PlayerDetailsPage: NextPage = (props: { player: Player }) => {
+  const player = props.player;
+
   return (
     <Page
       name={player && player.name}
       description={player ? `${player.name} (${player.position})` : ""}
-      loading={loading}
-      error={error}
     >
       {player && <PlayerDetails player={player} />}
     </Page>
