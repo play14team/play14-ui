17e233b23ffd73185c2e7d39db7b0a9f9488822b
Cédric PONTET <cpontet@agilepartner.net>
2023-03-26 21:29:32 +0200

    ignore timezone for now

--- components/events/time.tsx
+++ components/events/time.tsx
@@ -2,11 +2,7 @@ import Moment from "react-moment";
 import "moment-timezone";
 
 const EventTime = (props: { time: Date; timezone: string }) => {
-  return (
-    <Moment tz={props.timezone} format="ddd, MMM Do - HH:mm">
-      {props.time}
-    </Moment>
-  );
+  return <Moment format="ddd, MMM Do - HH:mm">{props.time}</Moment>;
 };
 
 export default EventTime;
