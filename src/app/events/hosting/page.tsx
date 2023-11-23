bb2af3c684a3ced05093c80db2d0fda51b16ac36
Cédric PONTET <cpontet@agilepartner.net>
2023-11-23 13:18:34 +0100

    migrated hosting

--- src/app/events/hosting/page.tsx
+++ src/app/events/hosting/page.tsx
@@ -1,3 +1,19 @@
-export default function Hosting() {
-  return <div>Hosting</div>
+import HtmlContent from "@/components/layout/html-content"
+import Page from "@/components/layout/page"
+import { getClient } from "@/libs/apollo-client"
+import { Hosting, HostingDocument } from "@/models/graphql"
+
+export default async function Hosting() {
+  const { data } = await getClient().query({ query: HostingDocument })
+  const hosting = data.hosting?.data?.attributes as Hosting
+
+  return (
+    <Page name="All you need to know about hosting a #play14 event">
+      <div className="pt-100 pb-70">
+        <div className="container">
+          <HtmlContent>{hosting.content!}</HtmlContent>
+        </div>
+      </div>
+    </Page>
+  )
 }
