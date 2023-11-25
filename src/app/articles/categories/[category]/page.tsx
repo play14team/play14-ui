f23ca66b32f5e12fc1ac416d05aef4a15909fa72
Cédric PONTET <cpontet@agilepartner.net>
2023-11-25 13:28:37 +0100

    feat: :sparkles: navigate articles by category and tags

---
+++ src/app/articles/categories/[category]/page.tsx
@@ -0,0 +1,23 @@
+import { getClient } from "@/libs/apollo-client"
+import ArticleGrid from "../../../../components/articles/grid"
+import Page from "../../../../components/layout/page"
+import { ArticleEntity, ArticlesDocument } from "../../../../models/graphql"
+
+export default async function ArticleCategory({
+  params,
+}: {
+  params: { category: string }
+}) {
+  const { data } = await getClient().query({
+    query: ArticlesDocument,
+    variables: { page: 1, pageSize: 1000, category: params.category },
+  })
+
+  const articles = data?.articles?.data as ArticleEntity[]
+
+  return (
+    <Page name={`Articles - Category : ${params.category}`}>
+      {data && <ArticleGrid articles={articles} />}
+    </Page>
+  )
+}
